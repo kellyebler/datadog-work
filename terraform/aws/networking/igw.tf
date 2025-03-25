900dad340f438c61dddc01d8e70d22df37ecf9a8
Kelly Ebler <kelly.ebler@gmail.com>
2025-03-25 09:22:14 -0500

    removing unnecessary tags

--- terraform/aws/networking/igw.tf
+++ terraform/aws/networking/igw.tf
@@ -2,8 +2,5 @@ resource aws_internet_gateway "kellyebler-igw" {
         vpc_id = aws_vpc.kellyebler-vpc.id
         tags = {
             Name = "kellyebler-igw"
-            please_keep_my_resource = "true"
-            team = "Enterprise Sales Engineering -- TOLA"
-            user = "KellyEbler-EC2"
         }
     }
